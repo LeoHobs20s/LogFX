e3dd49c0b6eadacf731d72eea2225892296eed3e
Leonardo Hobeni <leonardohobeni@gmail.com>
2026-01-12 11:15:11 +0200

    Added Messages, error_messages and help_text. Then I included some styling for the forms using bootstrap5

--- LogFX/settings.py
+++ LogFX/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third-party apps
+
     # my apps
     'forex_logs',
     'accounts',
--- accounts/forms.py
+++ accounts/forms.py
@@ -5,13 +5,14 @@ from django.contrib.auth.forms import UserCreationForm
 class LoginForm(forms.Form):
     """ This class will represent the login form """
 
-    username = forms.CharField(max_length=120)
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(max_length=120,widget=forms.TextInput(attrs={'class':'form-control w-50', 'placeholder':'Enter your username'}),
+                            help_text='120 Character max.', error_messages={'required':'Enter your username'})
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control w-50', 'placeholder':'Enter your password'}), error_messages={'required':'Enter your password'})
 
 
 class RegistrationForm(UserCreationForm):
     """ This class will represent the registration Forms """
 
-    username = forms.CharField(widget=forms.TextInput)
-    password1 = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control w-50', 'placeholder':'Enter a username'}), help_text='120 Characters max', error_messages={'required':'Enter a username'})
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control w-50', 'placeholder':'Enter a password'}), error_messages={'required':'Enter a password'}, label='Password')
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control w-50', 'placeholder':'Confirm Password'}), error_messages={'required':'Enter a password'}, label='Confirm Password')
